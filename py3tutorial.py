c24da829defd06cbb8b1b9ecb5fa34c26a6fa99c
Kyle Chen <kyle.h.chen@gmail.com>
2019-08-21 15:20:49 -0700

    Tuples vs Lists

---
+++ py3tutorial.py
@@ -0,0 +1,15 @@
+print("hello world")
+
+# this is a tuple as opposed to a list
+# syntax would be either written like this or with parantheses
+programming_languages = "Python", "Java", "C++", "C#"
+
+# an array would use brackets like this:
+languages_list = ["Python", "Java", "C++", "C#"]
+
+# for - in loop
+for language in programming_languages:
+    print(language)
+
+for language in languages_list:
+    print(language)
